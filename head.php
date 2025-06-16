db4028f77dc77942c910c4a0c02677fec7f1dc4a
Ben Dover <raphael.reinkensmeier@schueler.fesloe.de>
2025-06-16 16:50:50 +0200

    Add files via upload

--- head.php
+++ head.php
@@ -10,7 +10,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Mein PHP-Projekt</title>
-    <link rel="stylesheet" href="./css/styles.css">
+    <title>Casino Loyal</title>
+    <link rel="stylesheet" href="./styles.css">
 </head>
 <body>
